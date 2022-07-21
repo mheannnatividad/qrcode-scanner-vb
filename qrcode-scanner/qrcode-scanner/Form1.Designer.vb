1a7dfe4ccc0f5b9d0f96ee6f36058bfe25c10e97
mheannnatividad <maryannbernardo212@gmail.com>
2022-07-22 01:19:15 +0800

    add basic UI

---
+++ qrcode-scanner/qrcode-scanner/Form1.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.fullnameLabel = New System.Windows.Forms.Label()
+        Me.emailLabel = New System.Windows.Forms.Label()
+        Me.contactLabel = New System.Windows.Forms.Label()
+        Me.AddressLabel = New System.Windows.Forms.Label()
+        Me.courseSectionLabel = New System.Windows.Forms.Label()
+        Me.studentNumberLabel = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(210, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Person Information"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Window
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.AddressLabel)
+        Me.Panel1.Controls.Add(Me.contactLabel)
+        Me.Panel1.Controls.Add(Me.emailLabel)
+        Me.Panel1.Controls.Add(Me.studentNumberLabel)
+        Me.Panel1.Controls.Add(Me.courseSectionLabel)
+        Me.Panel1.Controls.Add(Me.fullnameLabel)
+        Me.Panel1.Location = New System.Drawing.Point(17, 37)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(289, 260)
+        Me.Panel1.TabIndex = 1
+        '
+        'fullnameLabel
+        '
+        Me.fullnameLabel.AutoSize = True
+        Me.fullnameLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.fullnameLabel.Location = New System.Drawing.Point(3, 9)
+        Me.fullnameLabel.Name = "fullnameLabel"
+        Me.fullnameLabel.Size = New System.Drawing.Size(97, 24)
+        Me.fullnameLabel.TabIndex = 0
+        Me.fullnameLabel.Text = "Fullname"
+        '
+        'emailLabel
+        '
+        Me.emailLabel.AutoSize = True
+        Me.emailLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.emailLabel.Location = New System.Drawing.Point(3, 109)
+        Me.emailLabel.Name = "emailLabel"
+        Me.emailLabel.Size = New System.Drawing.Size(62, 24)
+        Me.emailLabel.TabIndex = 0
+        Me.emailLabel.Text = "Email"
+        '
+        'contactLabel
+        '
+        Me.contactLabel.AutoSize = True
+        Me.contactLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.contactLabel.Location = New System.Drawing.Point(3, 144)
+        Me.contactLabel.Name = "contactLabel"
+        Me.contactLabel.Size = New System.Drawing.Size(80, 24)
+        Me.contactLabel.TabIndex = 0
+        Me.contactLabel.Text = "Contact"
+        '
+        'AddressLabel
+        '
+        Me.AddressLabel.AutoSize = True
+        Me.AddressLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AddressLabel.Location = New System.Drawing.Point(3, 181)
+        Me.AddressLabel.Name = "AddressLabel"
+        Me.AddressLabel.Size = New System.Drawing.Size(87, 24)
+        Me.AddressLabel.TabIndex = 0
+        Me.AddressLabel.Text = "Address"
+        '
+        'courseSectionLabel
+        '
+        Me.courseSectionLabel.AutoSize = True
+        Me.courseSectionLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.courseSectionLabel.Location = New System.Drawing.Point(3, 42)
+        Me.courseSectionLabel.Name = "courseSectionLabel"
+        Me.courseSectionLabel.Size = New System.Drawing.Size(153, 24)
+        Me.courseSectionLabel.TabIndex = 0
+        Me.courseSectionLabel.Text = "Course Section"
+        '
+        'studentNumberLabel
+        '
+        Me.studentNumberLabel.AutoSize = True
+        Me.studentNumberLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.studentNumberLabel.Location = New System.Drawing.Point(3, 76)
+        Me.studentNumberLabel.Name = "studentNumberLabel"
+        Me.studentNumberLabel.Size = New System.Drawing.Size(162, 24)
+        Me.studentNumberLabel.TabIndex = 0
+        Me.studentNumberLabel.Text = "Student Number"
+        '
+        'Panel2
+        '
+        Me.Panel2.Location = New System.Drawing.Point(322, 37)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(466, 271)
+        Me.Panel2.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(317, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 25)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Camera:"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(416, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 320)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Contact Tracing QR Code Scanner"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents AddressLabel As Label
+    Friend WithEvents contactLabel As Label
+    Friend WithEvents emailLabel As Label
+    Friend WithEvents studentNumberLabel As Label
+    Friend WithEvents courseSectionLabel As Label
+    Friend WithEvents fullnameLabel As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+End Class
